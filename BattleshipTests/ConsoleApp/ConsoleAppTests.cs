ff9f63ed7593464150f661d67e36ec23e61c5dc2
Robert Łyso <robertlyson@gmail.com>
2022-08-06 15:06:18 +0200

    additional tests for console app

--- BattleshipTests/ConsoleApp/ConsoleAppTests.cs
+++ BattleshipTests/ConsoleApp/ConsoleAppTests.cs
@@ -38,6 +38,44 @@ public class ConsoleAppTests
         Approvals.Verify(actual);
     }
 
+    [Test]
+    public void Play_game_with_invalid_input()
+    {
+        var console = new TestConsole();
+        console.Input.PushTextWithEnter("abc");
+        console.Input.PushTextWithEnter("a-1");
+        console.Input.PushTextWithEnter("Z9");
+        console.Input.PushTextWithEnter("B2");
+        console.Input.PushTextWithEnter("A1");
+
+        var game = new Game(new Ship[]
+        {
+            new SinglePositionShip("Foo", new Position('A', 1))
+        });
+        new App(console).Start(game);
+
+        var actual = console.Output;
+        Approvals.Verify(actual);
+    }
+
+    [Test]
+    public void Play_game_with_two_ships()
+    {
+        var console = new TestConsole();
+        console.Input.PushTextWithEnter("A1");
+        console.Input.PushTextWithEnter("J10");
+
+        var game = new Game(new Ship[]
+        {
+            new SinglePositionShip("Foo", new Position('A', 1)),
+            new SinglePositionShip("Bar", new Position('J', 10)),
+        });
+        new App(console).Start(game);
+
+        var actual = console.Output;
+        Approvals.Verify(actual);
+    }
+
     private static Game GameFixture()
     {
         return new Game(new Ship[] { new SinglePositionShip("Foo", new Position('A', 1)) });
